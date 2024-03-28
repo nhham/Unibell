cc07fcf1667cfa0c7fbb0b92867af0cd4ca4db6c
Nathaniel Ham <hambro06@gmail.com>
2024-03-28 10:49:12 -0500

    Initial Commit

---
+++ Unibell/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unibell
+//
+//  Created by hyunsuh ham on 3/28/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unibell/UnibellApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnibellApp.swift
+//  Unibell
+//
+//  Created by hyunsuh ham on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct UnibellApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
